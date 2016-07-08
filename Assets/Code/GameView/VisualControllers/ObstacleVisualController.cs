a50b83b18c0b120dcbbe5bd44f4d8639bcad0878
Daniel <daniel.maansson@gmail.com>
2016-07-08 11:41:16 +0200

    Cooler enemies

--- Assets/Code/GameView/VisualControllers/ObstacleVisualController.cs
+++ Assets/Code/GameView/VisualControllers/ObstacleVisualController.cs
@@ -12,6 +12,7 @@ public class ObstacleVisualController : DangerVisualController
 	GameObject m_root;
 
 	DangerView m_dangerView;
+	float m_timer;
 
 	public override void Init(DangerView dangerView)
 	{
@@ -20,6 +21,8 @@ public class ObstacleVisualController : DangerVisualController
 
 		m_truck.SetActive(m_dangerView.Danger.requiredAction == Player.Action.Right);
 		m_ball.SetActive(m_dangerView.Danger.requiredAction == Player.Action.Left);
+
+		m_timer = m_dangerView.GetExtrapolatedSecondsToImpact();
 
 		RefreshTransform();
 	}
@@ -31,17 +34,23 @@ public class ObstacleVisualController : DangerVisualController
 
 	void Update()
 	{
+		m_timer -= Time.deltaTime;
 		RefreshTransform();
 	}
 
 	void RefreshTransform()
 	{
-		float t = m_dangerView.GetExtrapolatedSecondsToImpact();
-		m_root.transform.localPosition = Vector3.right * t * (m_dangerView.Danger.requiredAction == Player.Action.Left ? -1f : 1f) * 80f;
+		m_root.transform.localPosition = Vector3.right * m_timer * (m_dangerView.Danger.requiredAction == Player.Action.Left ? -1f : 1f) * 80f;
 	}
 
 	void OnModelDangerDestroyed(DangerView dangerView)
 	{
-		dangerView.DestroyView();
+		StartCoroutine(DestroySoon(6f));
+	}
+
+	IEnumerator DestroySoon(float t)
+	{
+		yield return new WaitForSeconds(t);
+		m_dangerView.DestroyView();
 	}
 }
--- Assets/Code/GameView/VisualControllers/RobotVisualController.cs
+++ Assets/Code/GameView/VisualControllers/RobotVisualController.cs
@@ -12,6 +12,7 @@ public class RobotVisualController : DangerVisualController
 	GameObject m_left;
 
 	DangerView m_dangerView;
+	float m_timer;
 
 	public override void Init(DangerView dangerView)
 	{
@@ -20,22 +21,37 @@ public class RobotVisualController : DangerVisualController
 
 		m_right.SetActive(m_dangerView.Danger.requiredAction == Player.Action.Right);
 		m_left.SetActive(m_dangerView.Danger.requiredAction == Player.Action.Left);
+
+		m_timer = m_dangerView.GetExtrapolatedSecondsToImpact(); 
+		RefreshTransform();
 	}
 
 	void Start ()
 	{
 	
 	}
+
+	void RefreshTransform()
+	{
+		float t = m_timer;
+
+		m_root.transform.localPosition = Vector3.right * t * (m_dangerView.Danger.requiredAction == Player.Action.Left ? -1f : 1f) * 50f;
+	}
 	
 	void Update ()
 	{
-		float t = m_dangerView.GetExtrapolatedSecondsToImpact();
-
-		m_root.transform.localPosition = Vector3.right * t * (m_dangerView.Danger.requiredAction == Player.Action.Left ? -1f : 1f) * 50f;
+		m_timer -= Time.deltaTime;
+		RefreshTransform();
 	}
 
 	void OnModelDangerDestroyed(DangerView dangerView)
 	{
-		dangerView.DestroyView();
+		StartCoroutine(DestroySoon(0f));
+	}
+
+	IEnumerator DestroySoon(float t)
+	{
+		yield return new WaitForSeconds(t);
+		m_dangerView.DestroyView();
 	}
 }
